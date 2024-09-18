a83c29b879c6355ef839b20065f9256e7b90e30a
muhammad-danish <totalnetsolution2@gmail.com>
2024-09-19 03:21:39 +0500

    index file created

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: [
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+  extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+  };
+  
